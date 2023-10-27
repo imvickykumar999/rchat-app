a44a77650634dcfa765aa90169d1d25b6126c7c6
imvickykumar999 <imvickykumar999@gmail.com>
2023-10-27 13:30:52 +0530

    localhost
    https://www.freecodecamp.org/news/postgresql-in-python/

--- application.py
+++ application.py
@@ -9,13 +9,18 @@ from models import *
 
 # Configure app
 app = Flask(__name__)
-app.secret_key=os.environ.get('SECRET')
+app.secret_key='super secret key'
+
+# app.secret_key=os.environ.get('SECRET')
 app.config['WTF_CSRF_SECRET_KEY'] = "b'f\xfa\x8b{X\x8b\x9eM\x83l\x19\xad\x84\x08\xaa"
 
 # Configure database
-app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL')
+# app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL')
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+# db.init_app(app)
 
 # Initialize login manager
 login = LoginManager(app)
--- create.py
+++ create.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from models import *
 
+import os
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL')
